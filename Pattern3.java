e5a46e844e7241e25ea78edeb5a22786ba7652e0
Sanjay Bhandari <107557253+sanjaybhandari-github@users.noreply.github.com>
2022-07-29 21:28:45 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=13;//n-1/2=6
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				if( i-j>=(n-1)/2 || i+j>=n+(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
